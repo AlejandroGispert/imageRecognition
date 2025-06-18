07cf438381632ca45eda000d947c1afd3acf6d1a
Alejandro Gispert <alejandrobusiness2022@gmail.com>
2025-06-18 18:17:29 +0200

    [feat] classify images, unfinished

---
+++ classifyImages/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using Microsoft.Azure.CognitiveServices.Vision.CustomVision.Prediction;
+
+// Authenticate a client for the prediction API
+CustomVisionPredictionClient prediction_client = new CustomVisionPredictionClient(new ApiKeyServiceClientCredentials("<YOUR_PREDICTION_RESOURCE_KEY>"))
+{
+    Endpoint = "<YOUR_PREDICTION_RESOURCE_ENDPOINT>"
+};
+
+// Get classification predictions for an image
+MemoryStream image_data = new MemoryStream(File.ReadAllBytes("<PATH_TO_IMAGE_FILE>"));
+var result = prediction_client.ClassifyImage("<YOUR_PROJECT_ID>",
+                                             "<YOUR_PUBLISHED_MODEL_NAME>",
+                                             image_data);
+
+// Process predictions
+foreach (var prediction in result.Predictions)
+{
+    if (prediction.Probability > 0.5)
+    {
+        Console.WriteLine($"{prediction.TagName} ({prediction.Probability})");
+    }
+}
